f898febfe92c798f6326abb04854ba192cf45456
Mauricio Laine <laine.mau@gmail.com>
2020-01-21 10:48:49 -0800

    initialized the viewmodel in the Main Activity

--- app/src/main/java/com/laine/mauro/architecturecomponentsintro/MainActivity.kt
+++ app/src/main/java/com/laine/mauro/architecturecomponentsintro/MainActivity.kt
@@ -2,11 +2,17 @@ package com.laine.mauro.architecturecomponentsintro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var counterViewModel: CounterViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        counterViewModel = ViewModelProviders.of(this)[CounterViewModel::class.java]
     }
 }
