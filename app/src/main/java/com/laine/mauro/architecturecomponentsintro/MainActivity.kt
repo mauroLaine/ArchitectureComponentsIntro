856e8e956d89b03bf3a293c503b9d0e1e31442db
Mauricio Laine <laine.mau@gmail.com>
2020-01-21 11:44:52 -0800

    subscribe the live data counter intance, and updated the counter text view

--- app/src/main/java/com/laine/mauro/architecturecomponentsintro/MainActivity.kt
+++ app/src/main/java/com/laine/mauro/architecturecomponentsintro/MainActivity.kt
@@ -2,27 +2,45 @@ package com.laine.mauro.architecturecomponentsintro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var counterViewModel: CounterViewModel
+    private lateinit var counterLiveData: CounterLiveData
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //View Model initialization
         counterViewModel = ViewModelProviders.of(this)[CounterViewModel::class.java]
-        updateUI(counterViewModel.counter)
-
+        updateVMCounter(counterViewModel.counter)
         vm_increase_btn.setOnClickListener {
             counterViewModel.increaseCounter()
-            updateUI(counterViewModel.counter)
+            updateVMCounter(counterViewModel.counter)
         }
+
+        //Live Data implementation
+        counterLiveData = CounterLiveData()
+        subscribeLD()
+        ld_increase_btn.setOnClickListener {
+            counterLiveData.increaseCounter()
+        }
+
     }
 
-    fun updateUI(value: Int) {
+    fun updateVMCounter(value: Int) {
         vm_counter_tv.text = value.toString()
+    }
+
+
+    fun subscribeLD() {
+        val observer = Observer<Int> {
+            ld_counter_tv.text = it.toString()
+        }
+        counterLiveData.observe(this, observer)
     }
 }
